65755780b884ef9f100800e018038a8ee17d74e9
Tylar Murray <murray.tylar@gmail.com>
2012-05-24 23:04:13 -0400

    interrupt integration completed

--- SuperLoop/main.cpp
+++ SuperLoop/main.cpp
@@ -4,6 +4,18 @@
 //#include "msp430.h"
 //#include "io430.h"
 
+//hardware definitions
+#define BUTTON_BIT 0x40
+#define SENSOR_ENABLE_BIT 0x01
+#define SENSOR_BIT 0x80
+#define LED0 0x02
+#define LED1 0x04
+#define LED2 0x08
+#define LED3 0x10
+#define LED4 0x20
+
+int P1OUTMASK = SENSOR_ENABLE_BIT + LED0 + LED1 + LED2 + LED3 + LED4; 
+
 //=== function prototypes ====================================================
 void readButton();
 void readSensor();
@@ -11,12 +23,11 @@ void readSensor();
 void display(float value);
 
 void delay(unsigned long amount);
-void blink();
-void fadeOff();
+void fade(int nOn, int nOff);
 
 //=== constants =============================================================
 //For 24 reset loop:
-const unsigned long countsUntilReset = 840000; //assuming 1 cycle through 
+const unsigned int countsUntilReset = 8400; //assuming 1 cycle through 
 
 //for sensor reading:
 const int sensorSamples = 8315; /*number of samples to take when reading sensor
@@ -34,7 +45,7 @@ const int nButtonSamples = 5;
 const int deltaT = 1;  //time between samples
 
 //for display:
-const int nDisplayLoops = 50; //number of loops display stays on after activation
+const int nDisplayLoops = 10; //number of loops display stays on after activation
 
 //=== global vars ===========================================================
 unsigned long count = 0;  //number of loops completed
@@ -46,97 +57,53 @@ int displayLoopsLeft = nDisplayLoops; //display is on whenever displayLoopsLeft 
 
 int main( void )
 { 
+  // === STARTUP =======================
   WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer to prevent timeout reset
-  //CONFIGURE PORTS:
+  //===CONFIGURE PORTS:
   //config onboard LED on P1.0
-  P1DIR = 0x3F;   // set pins 1-6 for output, 7&8 for input
-  P1REN = 0x41;  //set p1.6 p1.0 (button) to use pull-down resistor
-  P1OUT = 0x00;  //everything in P1 OFF, pull-(up/down) on p1.6 & p1.0 set to down
-  
+  P1DIR = P1OUTMASK;  // set pins 1-6 for output, 7&8 for input
+  P1REN = BUTTON_BIT;  //set p1.6 (button) to use pull-down resistor
+  P1OUT = 0x00;  //everything in P1 OFF, pull-(up/down)s set to down
   //cycle LEDs to show successful startup
   for(int i = 3; i > 0; i--){
-   P1OUT = 0x20;  //pin 1.5 on (LED5)
+   P1OUT = LED4 & P1OUTMASK;  //pin 1.5 on (LED5)
    delay(10000);
-   P1OUT = 0x10;  //pin 1.4 on (LED4)
+   P1OUT = LED3 & P1OUTMASK;  //pin 1.4 on (LED4)
    delay(10000);
-   P1OUT = 0x08;  //pin 1.3 on (LED3)
+   P1OUT = LED2 & P1OUTMASK;  //pin 1.3 on (LED3)
    delay(10000);
-   P1OUT = 0x04;  //pin 1.2 on (LED2)
+   P1OUT = LED1 & P1OUTMASK;  //pin 1.2 on (LED2)
    delay(10000);
-   P1OUT = 0x02;  //pin 1.1 on (LED1)
+   P1OUT = LED0 & P1OUTMASK;  //pin 1.1 on (LED1)
    delay(10000);
-  // P1OUT = 0x01;  //pin 1.0 on (onboard LED)
   }
-  
-  //=== mainloop =============================================================
-  while(1){
-    //timer resets system every 24hrs
-      if(count > countsUntilReset){
-        WDTCTL = 0; //restart
-      } //implied else
-      count++;
-
-      readButton();
-      if(buttonState == true){  //if button pressed, count amount of time held
-        //count time button is held
-        long onCount = 0;
-        do{ 
-          readButton();
-          onCount++;
-          if(onCount > 10000){ //if held a long time
-            WDTCTL = 0; //restart
-          } //implied else
-        } while(buttonState == true);
-        //if button pressed & released
-          displayLoopsLeft = nDisplayLoops;
-      }
- 
-     readSensor();
-    
-    //add sensor reading to total
-      float freqScaler = 0.01;  //scales down total to kHz
-      total += sensorValue*freqScaler;
-    
-      if(displayLoopsLeft > 0){
-      //onboard LED on
-      //P1OUT |= 0x01;
-      //update display 
-      displayLoopsLeft--;
-      display(total);
-      //fade off display if time is up
-      if(displayLoopsLeft == 0){
-        fadeOff();
-        displayLoopsLeft = -1;
-      }
-    }
-  }
-  //=== FOR THE TIMER ==============
-  //TODO: what is this?
+  P1OUT = 0x00 & P1OUTMASK;   //clear display
+  //===SETUP THE TIMER
+  //TODO: what is this? TimerA Control = TimerA???_2 + MC??_2 + TimerAInterruptEnable
   TACTL = TASSEL_2 + MC_2 + TAIE;   //SMCLK, contmode, interrupt
-  
-  
-  //=== FOR THE BUTTON ==============
+  //===SETUP THE BUTTON
   //TODO: this should be set to the pin on which the button is located (P1.7???)
-  P1IE |= 0x10; //enable interrupt on P1.4
-  P1IES|= 0x10; //set interrupt mode on P1.4 to detect Hi/low edge
-  P1IFG&=~0x10; //clear interrupt flag on P1.4
-  
-   //this unreachable line puts the MSP430 into low-power 4 (just in case)
+  P1IE |= BUTTON_BIT; //enable interrupt on P1 button
+  P1IES&=~BUTTON_BIT; //set interrupt mode on P1 to detect LOW-HIGH edge
+  P1IFG&=~BUTTON_BIT; //clear interrupt flag on P1
+  //put the MSP430 into low-power mode
+  while(1){
   __bis_SR_register(LPM0_bits + GIE); //enter low-power 0 w/ interrupt
+  }
 }
 
 void readButton(){
   bool sameFlag = true; //flag for all samples have same value
   //take samples at interval for compare (debounce)
-  buttonSamples[0] = P1IN & 0x40;
+  buttonSamples[0] = P1IN & BUTTON_BIT;
   for(int i = 1; i < nButtonSamples; i++){
-    buttonSamples[i] = P1IN & 0x40;
+    buttonSamples[i] = P1IN & BUTTON_BIT;
     if(buttonSamples[i] != buttonSamples[i-1]){
       sameFlag = false;
     }
     delay(deltaT);
   }
-  if(sameFlag && (buttonSamples[0] == 0x40)){ //if all samples on
+  if(sameFlag && (buttonSamples[0] == BUTTON_BIT)){ //if all samples on
     buttonState = true;
   }
   if(sameFlag && (buttonSamples[0] == 0x00)){//if all samples off
@@ -145,16 +112,15 @@ void readButton(){
   //else button remains in previous state   
   return;
 }
-
-void readSensor(){     /*returns frequency reading from sensor*/
+void readSensor(){     //returns frequency reading from sensor
   int n = 0;   //number of signal on/off and off/on changes read
   int lastRead = 0;
   //initial read
-  int thisRead = P1IN & 0x80;
+  int thisRead = P1IN & SENSOR_BIT;
   //for sample time
   for(int i = sensorSamples; i > 0; i--){
     lastRead = thisRead;  //push back previous read
-    thisRead = P1IN & 0x80; //read sensor
+    thisRead = P1IN & SENSOR_BIT; //read sensor
     if(thisRead != lastRead){
       n++;
     }
@@ -163,47 +129,40 @@ void readSensor(){     /*returns frequency reading from sensor*/
   return;
 }
 
-void display(float value){  /*updates display with value*/
+void display(float value){  //updates display with value
   float bin0 = 1000;  //no LEDS on iff lower than bin0
   float bin1 = 2000;
   float bin2 = 3000;
   float bin3 = 4000;
   float bin4 = 5000; //higher than bin4 means all LEDS on
   if(value < bin0){
-    P1OUT = 0x02;
+    P1OUT = LED0 & P1OUTMASK;
   }else if(value < bin1){
-    P1OUT = 0x06;
+    P1OUT = (LED0 + LED1) & P1OUTMASK;
   }else if(value < bin2){
-    P1OUT = 0x0E;
+    P1OUT = (LED0 + LED1 + LED2) & P1OUTMASK;
   }else if(value < bin3){
-    P1OUT = 0x1E;
+    P1OUT = (LED0 + LED1 + LED2 + LED3) & P1OUTMASK;
   }else if(value < bin4){
-    P1OUT = 0x1E;
+    P1OUT = (LED0 + LED1 + LED2 + LED3 + LED4) & P1OUTMASK;
   }else{
-    P1OUT = 0x3E;
+    //flash if value is this high
+    P1OUT ^= (LED0 + LED2 + LED4) & P1OUTMASK;
   }
   
   return;
 }
 
-/* very simple, power-wasting delay
- * units of 'amount' is CPU cycles
- */
+// very simple, power-wasting delay
+// *NOTE: units of 'amount' is CPU cycles
 void delay(unsigned long amount){ 
   for(unsigned long i = amount; i > 0; i--);
 }
 
-void blink(){
-  P1OUT = 0xFF;
-  delay (2000);
-  P1OUT = 0x00;
-  delay (2000);
-}
-
-void fadeOff(){   //fade using crude PWM
+//fade using crude PWM
+//fade(int initialTimeOn, int endingTimeOn)
+void fade(int nOn, int nOff){   
   int P1state = P1OUT;
-  int nOn = 500;
-  int nOff = 0;
   for(int i = nOn + nOff; i > 0; i--){
     nOn --;
     nOff ++;
@@ -225,19 +184,73 @@ __interrupt void Timer_A(void){
   switch(TAIV){
   case 2: break;  //CCR1 not used
   case 4: break;  //CCR2 not used
-  case 10: P1OUT ^= 0x01;   //overflow
+  //case 10: P1OUT ^= 0x01 & P1OUTMASK;   //overflow
     break;
   }
+  //add to runtime clock
+  count ++;
+  //timer resets system every 24hrs
+  if(count > countsUntilReset){
+    WDTCTL = 0; //restart
+  } //implied else
   
-  //TODO: move sample read, display update code here
+  if (count % 10 == 0){
+      // === TAKE SENSOR READING =============
+    P1OUT |= SENSOR_ENABLE_BIT & P1OUTMASK;  //turn sensor on
+    readSensor();
+    P1OUT ^= SENSOR_ENABLE_BIT & P1OUTMASK; //turn sensor off
+    //add sensor reading to total
+    float freqScaler = 0.01;  //scales down total to kHz
+    total += sensorValue*freqScaler;
+    
+    // === UPDATE DISPLAY ==================
+    if(displayLoopsLeft > 0){
+      //onboard LED on
+      //P1OUT |= 0x01;
+      //update display 
+      displayLoopsLeft--;
+      display(total);
+      //fade off display if time is up
+      if(displayLoopsLeft == 0){
+        fade(200, 0);
+        displayLoopsLeft = -1;
+      }
+    }
+  }
 }
+
 
 //Port 1 interrupt service routine (BUTTON)A
 #pragma vector=PORT1_VECTOR
 __interrupt void Port_1(void){
   //TODO: handle the button press here
   
+  //test blink when pressed
+  P1OUT  = (LED0 + LED1 + LED2 + LED3 + LED4) & P1OUTMASK; // P1 toggle
   
+  readButton();
+
+ /*
+  P1IES ^= BUTTON_BIT; // toggle the interrupt edge,
+  // the interrupt vector will be called
+  // when P1.3 goes from HitoLow as well as
+  // LowtoHigh
+  */
   
+  if(buttonState == true){  //if button pressed, count amount of time held
+      //count time button is held
+      long onCount = 0;
+      do{ 
+        readButton();
+        onCount++;
+        if(onCount > 10000){ //if held a long time
+          WDTCTL = 0; //restart
+        } //implied else
+      } while(buttonState == true);
+      //if button pressed & released
+      displayLoopsLeft = nDisplayLoops;
+    }
+  
+  P1IFG &= ~BUTTON_BIT; // P1.3 IFG cleared
 }
 
